9ad8f1ee331a932aed5e23e497f4e99f2c002e87
Diego Soto Letelier <diego.soto.l02@mail.pucv.cl>
2019-12-18 08:49:34 -0300

    se agrego el index

---
+++ python-basics/list.py
@@ -0,0 +1,31 @@
+#crear lista
+demo_list = [1,'hello',1.34,True,[1,2,3]]
+colores = ['rojo','negro','blanco']
+
+#constructor
+lista_numerica = list((1,2,3,4))#se le entrega una tupla a la lista
+
+# print(lista_numerica)
+# print(type(lista_numerica))
+# r= list((range(0,100)))#se genera una lista(0-99) con valores continuos
+# print(r)
+# print(len(colores))#cantidad de elementos de la lista
+# print(colores[0])#imprime segun indices de una lista
+# print('rojo' in colores)
+
+# colores[0] = 'cafe'
+# colores.append('violeta') #agregar elemento
+# colores.extend({'gris', 'azul'}) #agregar una lista por separado
+# colores.insert(1,'azul') #inserta un nuevo elemento en una posicion
+# colores.insert(len(colores), 'nuevo_color')#agrega alfinal de la lista
+
+# colores.pop(0) #elimina el elemento segun indice
+# colores.pop() #elimina el ulrimo elemento de la lista
+# # colores.remove('blanco') #eliminar de la lista basado en el dato
+# colores.clear() #limpia lalista
+
+# colores.sort() #ordena alfabeticamente
+# colores.sort(reverse=True) #ordena de manera inversa
+
+# # print(colores.index('blanco'))#imprime el indice del dato
+# print(colores.count('rojo')) #cantidad de elementos
