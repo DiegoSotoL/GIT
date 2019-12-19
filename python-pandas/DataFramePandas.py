c1d8b8a47ff6f8e277e2111d1cc33e299f0e6fdb
Diego Soto Letelier <diego.soto.l02@mail.pucv.cl>
2019-12-19 06:52:41 -0300

    dataframe_pandas

--- python-pandas/DataFramePandas.py
+++ python-pandas/DataFramePandas.py
@@ -23,15 +23,3 @@ import matplotlib.pyplot as plt
 
 # #imprime los datos de un tipo especifico
 # print(df.loc[df['Type 1']  == "Fire"] )
-
-
-from pandas import DataFrame
-   
-Data = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
-        'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
-       }
-  
-df = DataFrame(Data,columns=['Year','Unemployment_Rate'])
-print (df)	
-
-df.plot(x ='Year', y='Unemployment_Rate', kind = 'line')
