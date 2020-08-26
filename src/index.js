8791a14392afbce72ebda1314caec303b5240cf5
Diego Soto <diego.soto.l02@mail.pucv.cl>
2020-08-26 05:15:34 -0400

    configuracion servidor

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+//Init
+const app = express();
+//Config
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs',exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+
+}));
+app.set('view engine', '.hbs');
+
+//Middlewares}
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
+//Global
+app.use((req,res,next)=>{
+    next();
+});
+
+//Rutas
+app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+
+//Public
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Iniciar
+app.listen(app.get('port'), ()=> {
+    console.log('Server en puerto', app.get('port'));
+});
